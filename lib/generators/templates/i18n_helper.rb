d8454e0271fc7f12fb20f1f2d870bbcaf0dbdf84
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2016-12-21 21:59:10 +0100

    Added comments to the i18n_helper generator template.

--- lib/generators/templates/i18n_helper.rb
+++ lib/generators/templates/i18n_helper.rb
@@ -1,5 +1,11 @@
 I18nHelper.configure do |config|
-  # Set some options here
+  # Enables overriding the values returned by the I18n translation method.
+  config.enabled = true
+
+  # The keys for which the the gem will return the original values.
+  # You can use strings or regular expressions here.
+  config.skip_keys = []
 end
 
+# Adds custom translation code to the I18n gem.
 I18n.include I18nHelper::I18nExtension
