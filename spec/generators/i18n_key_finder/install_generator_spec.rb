338bd41fbfd72e13363086574eff7a144aeb21de
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2017-01-10 22:29:02 +0100

    Added specs for the install_generator.

---
+++ spec/generators/i18n_key_finder/install_generator_spec.rb
@@ -0,0 +1,15 @@
+require 'generator_spec'
+require 'generators/i18n_key_finder/install_generator'
+
+describe I18nKeyFinder::Generators::InstallGenerator, type: :generator do
+  destination File.expand_path("../../tmp", __FILE__)
+
+  before(:all) do
+    prepare_destination
+    run_generator
+  end
+
+  it 'creates the initializers file' do
+    assert_file 'config/initializers/i18n_key_finder.rb'
+  end
+end
