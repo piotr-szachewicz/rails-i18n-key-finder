b82d19ed83aa20b16e9530d79d16f924dbd56f9d
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2017-01-14 12:31:31 +0100

    Added i18n_key_finder to the dummy app.

---
+++ spec/dummy/config/initializers/i18n_key_finder.rb
@@ -0,0 +1,10 @@
+I18nKeyFinder.configure do |config|
+  # The keys for which the the gem will return the original values.
+  # You can use strings or regular expressions here.
+  config.skip_keys = [/format/, /transliterate/]
+end
+
+# Adds custom translation code to the I18n gem.
+# Note: the keys will only be shown if the SHOW_I18N_KEYS
+# environment variable is set to true.
+I18n.include I18nKeyFinder::I18nExtension
